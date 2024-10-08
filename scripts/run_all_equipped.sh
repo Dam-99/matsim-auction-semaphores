9deee14e91d0f5638faad5bf9eee767c63f7061d
Damiano Mason <57217353+Dam-99@users.noreply.github.com>
2024-10-08 15:03:03 +0200

    fix: correct server for communication and proportional in scripts

--- scripts/run_all_equipped.sh
+++ scripts/run_all_equipped.sh
@@ -8,10 +8,16 @@ for mode in 'fixed' 'basic' 'communication' 'proportional'; do
             else
                 mode_output=$mode
             fi
+            if [ $mode == "communication" ] || [ $mode == "proportional" ]; then
+                propagated="_prop"
+            else
+                propagated=""
+            fi
             sed -i -e "s/TO_SET_AGENTS/${agents}/g" ./configs/config.xml
             sed -i -e "s/TO_SET_NUMBER/${i}/g" ./configs/config.xml
             sed -i -e "s/TO_SET_MODE/${mode}/g" ./configs/config.xml
             sed -i -e "s/TO_SET_OUTMODE/${mode_output}/g" ./configs/config.xml
+            sed -i -e "s/_PROPAGATION/${propagated}/g" ./configs/config.xml
             java -Xmx6g -cp ./target/smartcity-0.0.1-SNAPSHOT.jar org.matsim.contrib.smartcity.RunSmartcity ./configs/config.xml
             cp ./configs/config_template_all_equipped.xml ./configs/config.xml
         done
--- scripts/run_coexist.sh
+++ scripts/run_coexist.sh
@@ -21,12 +21,18 @@ do
                 else
                     mode_output=$mode
                 fi
+                if [ $mode == "communication" ] || [ $mode == "proportional" ]; then
+                    propagated="_prop"
+                else
+                    propagated=""
+                fi
                 sed -i -e "s/TO_SET_TRAFFIC/${traffic}/g" ./configs/config.xml
                 sed -i -e "s/TO_SET_OUTTRAFFIC/${traffic_output}/g" ./configs/config.xml
                 sed -i -e "s/TO_SET_AGENTS/${agents}/g" ./configs/config.xml
                 sed -i -e "s/TO_SET_NUMBER/${i}/g" ./configs/config.xml
                 sed -i -e "s/TO_SET_MODE/${mode}/g" ./configs/config.xml
                 sed -i -e "s/TO_SET_OUTMODE/${mode_output}/g" ./configs/config.xml
+                sed -i -e "s/_PROPAGATION/${propagated}/g" ./configs/config.xml
                 java -Xmx6g -cp ./target/smartcity-0.0.1-SNAPSHOT.jar org.matsim.contrib.smartcity.RunSmartcity ./configs/config.xml
                 cp ./configs/config_template_coexist.xml ./configs/config.xml
             done
